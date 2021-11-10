939191dcc7cafb1abf4cb867df3f9bffcf639540
kk0903 <81209588+kk0903@users.noreply.github.com>
2021-11-10 21:25:57 +0800

    Update testModuel.java

--- testModuel.java
+++ testModuel.java
@@ -5,6 +5,6 @@ public class testModuel {
         moduleB.setModuelA(moduelA);
         System.out.println("计科182");
         System.out.println("测试一："+moduleB.operate("45","454545"));
-        System.out.println("测试结果"+moduleB.operate("zzhnb","zzhnb"));
+        System.out.println("测试结果："+moduleB.operate("lizitian","zzhnb"));
     }
 }
