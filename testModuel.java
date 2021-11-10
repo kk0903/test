df0f7206de7aaa3d44d5d384a2a0ea61974c994e
kk0903 <81209588+kk0903@users.noreply.github.com>
2021-11-10 22:42:03 +0800

    Update testModuel.java

--- testModuel.java
+++ testModuel.java
@@ -3,7 +3,7 @@ public class testModuel {
         ModuelA moduelA = new ModuelA();
         ModuleB moduleB = new ModuleB();
         moduleB.setModuelA(moduelA);
-        System.out.println("测试一："+moduleB.operate("12","12345"));
-        System.out.println("测试二："+moduleB.operate(" zhuoyipeng"," zhuoyipeng"));
+        System.out.println("测试一："+moduleB.operate("66","66666"));
+        System.out.println("测试二："+moduleB.operate("zhangruixiang","zhangruixiang"));
     }
 }
