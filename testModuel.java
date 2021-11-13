b1817c953e2ea80fa4d57d1f749ba7ca78c2547e
kk0903 <81209588+kk0903@users.noreply.github.com>
2021-11-13 20:59:39 +0800

    Update testModuel.java

--- testModuel.java
+++ testModuel.java
@@ -3,6 +3,6 @@ public class testModuel {
         ModuelA moduelA = new ModuelA();
         ModuleB moduleB = new ModuleB();
         moduleB.setModuelA(moduelA);
-        System.out.println("测试用例：+"str1:heyongkang\n"+"str2:heyongkang"+moduleB.operate("zhangruixiang","zhangruixiang"));
+        System.out.println("测试用例:"+"str1:heyongkang\n"+"str2:heyongkang"+moduleB.operate("zhangruixiang","zhangruixiang"));
     }
 }
